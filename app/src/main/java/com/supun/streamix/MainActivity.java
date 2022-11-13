be1fa9395bedc22b7120d7486cf6a38d8c4ff799
supun64 <l.w.supunmadusanka64@gmail.com>
2022-11-13 10:29:27 +0530

    Initial Build

---
+++ app/src/main/java/com/supun/streamix/MainActivity.java
@@ -0,0 +1,50 @@
+package com.supun.streamix;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.supun.streamix.ui.main.SectionsPagerAdapter;
+import com.supun.streamix.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(view -> {
+            try{
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_VIDEO_CAPTURE);
+//                intent.putExtra() : Pass extras to the new Activity
+                startActivity(intent);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+    }
+}
