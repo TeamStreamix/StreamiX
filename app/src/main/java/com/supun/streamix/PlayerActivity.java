02abe2d63e6b821b96697eef2b8f6453ac58f4ac
supun64 <l.w.supunmadusanka64@gmail.com>
2022-12-02 01:33:51 +0530

    Successfully implemented the Manual resolution selector

--- app/src/main/java/com/supun/streamix/PlayerActivity.java
+++ app/src/main/java/com/supun/streamix/PlayerActivity.java
@@ -10,15 +10,20 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.Tracks;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionOverride;
+import com.google.android.exoplayer2.trackselection.TrackSelectionParameters;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.ui.StyledPlayerView;
@@ -26,7 +31,8 @@ import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.util.MimeTypes;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends AppCompatActivity
+ implements View.OnClickListener{
 
 
     ExoPlayer player;
@@ -35,6 +41,8 @@ public class PlayerActivity extends AppCompatActivity {
 
     Handler handler;
     Runnable runnable;
+
+    private boolean isShowingTrackSelectionDialog;
 
 
     @Override
@@ -54,6 +62,7 @@ public class PlayerActivity extends AppCompatActivity {
         playerView.setControllerShowTimeoutMs(3000);
 
         btnVideoQuality  =  findViewById(R.id.video_quality);
+        btnVideoQuality.setOnClickListener(this);
         videoTitle = findViewById(R.id.video_title);
 
         videoTitle.setText(videoName);
@@ -111,5 +120,17 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         player.stop();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnVideoQuality && !isShowingTrackSelectionDialog && TrackSelectionDialog.willHaveContent(player)) {
+            isShowingTrackSelectionDialog = true;
+            TrackSelectionDialog trackSelectionDialog =
+                    TrackSelectionDialog.createForPlayer(
+                            player,
+                            /* onDismissListener= */ dismissedDialog -> isShowingTrackSelectionDialog = false);
+            trackSelectionDialog.show(getSupportFragmentManager(), /* tag= */ null);
+        }
     }
 }
